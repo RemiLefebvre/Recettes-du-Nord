1a9823f274cfba3d4820564124ff7ea540adf78b
remi <lefebvre.reminicolas@gmail.com>
2018-02-07 14:00:43 +0100

    page recettes done

--- src/rdn/SiteBundle/Controller/HomeController.php
+++ src/rdn/SiteBundle/Controller/HomeController.php
@@ -44,16 +44,39 @@ class HomeController extends Controller
                                                                                 'recettesTop' => $recettesTop,
                                                                                 'recettesBottom' => $recettesBottom,
                                                                               ));
-
-        // echo "<pre>";
-        // die(var_dump($recettesTop));
         return new Response($content);
     }
 
 
     public function recettesAction()
     {
-        $content = $this->renderView('rdnSiteBundle:Home:recettes.html.twig',array('nom' => 'winzou'));
+
+      // On récupère le repository
+        $repository = $this->getDoctrine()
+          ->getManager()
+          ->getRepository('rdnSiteBundle:Home')
+        ;
+
+        $recettes = $repository->findBy(
+          array(), // Critere
+          array('name' => 'ASC')        // Tri                             // Offset
+        );
+
+        // $recettes est donc une instance de OC\PlatformBundle\Entity\Advert
+        // ou null si l'id $id  n'existe pas, d'où ce if :
+        if (null === $recettes) {
+          // A CHANGER >> ALLER VERS CONTOLLEUR ACCUEIL
+          $content = $this->renderView('rdnSiteBundle:Home:index.html.twig',array('nom' => 'winzou'));
+          return new Response($content);
+        }
+
+
+
+        // echo "<pre>";
+        // die(var_dump($oui));
+
+
+        $content = $this->renderView('rdnSiteBundle:Home:recettes.html.twig',array('recettes' => $recettes));
 
         return new Response($content);
     }
@@ -67,9 +90,7 @@ class HomeController extends Controller
           ->getRepository('rdnSiteBundle:Home')
         ;
 
-        // On récupère l'entité correspondante à l'id $id
         $recettes = $repository->find($id);
-        // die("oui");
 
         // $recettes est donc une instance de OC\PlatformBundle\Entity\Advert
         // ou null si l'id $id  n'existe pas, d'où ce if :
@@ -80,7 +101,6 @@ class HomeController extends Controller
           return new Response($content);
         }
 
-        // die(var_dump($recettes));
         $content = $this->renderView('rdnSiteBundle:Home:recette.html.twig',array('recette' => $recettes));
 
         return new Response($content);
