049335904f4e342b1a96021c7223db847dac02f0
remi <lefebvre.reminicolas@gmail.com>
2018-05-10 20:29:42 +0200

    recup (add ip 1/2)

--- src/rdn/SiteBundle/Controller/HomeController.php
+++ src/rdn/SiteBundle/Controller/HomeController.php
@@ -198,6 +198,16 @@ class HomeController extends Controller
         $message->setEmail($post['email']);
         $message->setMessage($post['message']);
 
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        $message->setIp($ip);
+
         // On récupère l'EntityManager
          $em = $this->getDoctrine()->getManager();
 
